9b8f3cfc2fd2a156a8ec458b2af3cb20c324394c
eSkortz <danyax4@mail.ru>
2024-07-10 17:31:38 +0300

    updating with docker image

--- application_telegram/env_reader.py
+++ application_telegram/env_reader.py
@@ -8,8 +8,8 @@ class AppConfig(BaseSettings):
     DB_PASSWORD: SecretStr
     BOT_TOKEN: SecretStr
     
-    class Config:
-        env_file = '.env'
-        env_file_encoding = 'utf-8'
+    # class Config:
+    #     env_file = '.env'
+    #     env_file_encoding = 'utf-8'
         
 app_config = AppConfig()
